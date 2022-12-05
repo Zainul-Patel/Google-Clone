5df46f5f2f947266a4d834bfea50a92137cf1ea1
Zainul Patel <u20521295@tuks.co.za>
2022-12-05 16:27:25 +0200

    sa

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Google Clone</h1>
+      <h1 className="text-3xl font-bold underline" >Google Clone </h1>
     </div>
   )
 }
