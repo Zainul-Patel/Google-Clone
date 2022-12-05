963cfd3625d55e53bfe32e53c6dfa8e126c5477c
Zainul Patel <u20521295@tuks.co.za>
2022-12-05 16:24:38 +0200

    za

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export const App = () => {
+const App = () => {
   return (
     <div>
-        <h1>App</h1>
+      <h1>Google Clone</h1>
     </div>
   )
 }
-//stopped at 7.36 of react video 
-//https://www.youtube.com/watch?v=NDbruK1fzG8&list=PL6QREj8te1P6wX9m5KnicnDVEucbOPsqR&index=29
+
+export default App
